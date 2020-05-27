1e8777423312c1d187bb51484b01c7a8abb9d7b7
Fran Green <fran@engineheadstudio.co.uk>
2020-05-27 18:04:53 +0100

    Add files via upload

---
+++ eh-scripts.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+   $(document).ready(function() {
+	   $(document).on ('click' , '.content' , function(e) {
+		   e.preventDefault();
+		   
+		   var category= $(this).data('category');
+		    $.ajax({
+				url: wpAjax.ajaxUrl,
+				data: { action: 'filter', category: category },
+				type: 'post',
+				success: function (result) {
+					$('.content').html(result);
+				},
+					
+					error: function(result) {
+						console.warn(result);
+					}
+					
+				});
+				
+			});
+	   });
+	   
+   }) (jQuery);
