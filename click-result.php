89881304d50c961c9b72cf3d21f3b12dcdc6dcbd
Fran Green <fran@engineheadstudio.co.uk>
2020-06-03 19:21:27 +0100

    Update click-result.php

--- click-result.php
+++ click-result.php
@@ -3,16 +3,19 @@
 add_action('wp_ajax_no_priv_filter' , 'filter_ajax');
 add_action('wp_ajax_filter' , 'filter_ajax');
 
+?>
 
-function filter_ajax(){
-	     
+	 	
+
+	 
+<?php function filter_ajax(){
 		 
 			 
-         $category = $_POST['category'];   		 
+         $category = $_POST['category'];   	 
 		 
 		 $args=array(
-				         'post_type'=>'classroom',
-						 'posts_per_page'=> -1
+				         'post_type' => 'classroom',
+						 'posts_per_page' => -1
 						 
 						 
 					);
@@ -25,41 +28,7 @@ function filter_ajax(){
 				  }
 				  
 				  
-				  				  
-				  
-				  $query = new WP_Query($args);
-				  
-				  if ($query->have_posts()) :
-				    while ($query->have_posts()) : $query->the_post();?>
-					
-					
-					
-<?php
-
-add_action('wp_ajax_no_priv_filter' , 'filter_ajax');
-add_action('wp_ajax_filter' , 'filter_ajax');
-
-
-function filter_ajax(){
-	     
-		 
-			 
-         $category = $_POST['category'];   		 
-		 
-		 $args=array(
-				         'post_type'=>'classroom',
-						 'posts_per_page'=> -1
-						 
-						 
-					);
-				  
-				  if(isset($category)) {
-					  
-					  $args['category__in'] = array($category);
-					
-					  
-				  }
-				  
+			  
 				  
 				  				  
 				  
@@ -69,9 +38,8 @@ function filter_ajax(){
 				    while ($query->have_posts()) : $query->the_post();?>
 					
 					
-					
 
-						<!---------click result------->
+						<!--------- click result ONE------->
 												
 									
 					<div class="posts-boxes">
@@ -87,6 +55,8 @@ function filter_ajax(){
 					  </div>
 												 
 				    </div>
+					
+					
 				  
 				    <?php endwhile;
 				          endif;
@@ -98,32 +68,8 @@ function filter_ajax(){
 	    die();
 }
 
-?>
-						<!---------click result------->
-												
-									
-					<div class="posts-boxes">
-					
-					  <div class="posts-boxes-inner">
-					
-					      <div class="post-box-thumbnail"><?php the_post_thumbnail(); ?></div>
-						
-						  <div class="post-box-custom-fields"><?php the_category(); ?></div>
-						
-						  <div class="post-box-title"><?php the_title('<h2>' , '</h2>'); ?></div>
-						
-					  </div>
-												 
-				    </div>
-				  
-				    <?php endwhile;
-				          endif;
-				          wp_reset_postdata();
-	
-	
-	
-	
-	    die();
-}
+
+
+
 
 ?>
